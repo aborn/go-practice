f31d34ba5fffe24530f1da9027bcea02d92750e7
Aborn Jiang <loveaborn@foxmail.com>
2017-02-12 00:08:21 +0800

    finished map

--- basic/round_map.go
+++ basic/round_map.go
@@ -1,13 +1,32 @@
 package basic
 
-import ()
+import (
+	"fmt"
+)
 
 var roundMap map[int]int
 
-func put(rm map[int]int) {
+const RoundMapMaxSize int = 2 * 1000
 
+func abs(value int) int {
+	if value < 0 {
+		return -value
+	}
+	return value
+}
+
+func Put(key int, value int) {
+	currentIndex := key % RoundMapMaxSize
+	roundMap[currentIndex] = value
+	for i := 0; i < RoundMapMaxSize; i++ {
+		if abs(i-currentIndex) >= RoundMapMaxSize/2 {
+			delete(roundMap, abs(i-currentIndex))
+		}
+	}
+	fmt.Println("current map size = ", len(roundMap))
 }
 
 func init() {
 	roundMap = make(map[int]int, 2000)
+	fmt.Println("init map size = ", len(roundMap))
 }
--- main.go
+++ main.go
@@ -1,9 +1,13 @@
 package main
 
 import (
+	"./basic"
 	"log"
 )
 
 func main() {
+	for i := 0; i < 2000; i++ {
+		basic.Put(i, i+1)
+	}
 	log.Println("Hello Go !")
 }
